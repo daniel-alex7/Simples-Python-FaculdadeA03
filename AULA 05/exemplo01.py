4a5e3ec61408695d6791972181e66944b3516b5b
daniel-alex7 <daniel456alexandre@gmail.com>
2025-03-26 19:37:56 -0300

    Aula do dia 26/03 mais alguns exemplos de condições

---
+++ AULA 05/exemplo01.py
@@ -0,0 +1,14 @@
+m1 = float(input("Digite a media final do aluno: "))
+f1 = float(input("Digite a frequência do aluno em %: "))
+
+if f1 < 75:
+    print("Reprovado por falta")
+    print("Sua frequência foi {} e sua nota {}".format(f1, m1))
+    
+else:
+    if m1 < 6: 
+        print("Reprovado por nota sua nota foi: {} e sua frequência {}".format(m1,f1))
+    else:
+        print("Aprovado")
+        print("Com média {} e frequência {}".format(m1,f1))
+     
