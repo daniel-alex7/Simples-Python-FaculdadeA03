1ba2ef80ccfe2b82b0abc3b81c7278523da20267
Daniel Alexandre <130123798+daniel-alex7@users.noreply.github.com>
2025-04-09 20:23:21 -0300

    mais códigos

---
+++ AULA 07/ex01D.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um número inteiro: "))
+
+for i in range(11): #range é usado em contagem
+ print(f'{num} x {i} = {num * i}')
+    
---
+++ AULA 07/ex02.py
@@ -0,0 +1,11 @@
+soma = 0.0
+
+for i in range(10):
+    num = float(input(f'Digite um número {i + 1}:  '))
+
+    soma = soma + num
+
+print(f'O resultado da soma é: {soma}')
+   
+
+
