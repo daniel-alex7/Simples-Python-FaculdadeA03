212b87f8e0947444bc47046f907247270d7c96dc
Daniel Alexandre <130123798+daniel-alex7@users.noreply.github.com>
2025-04-09 20:57:07 -0300

    Incrementado outros laços de repetição

---
+++ AULA 07/ex03.py
@@ -0,0 +1,12 @@
+contador = 1
+
+while contador <= 10:
+    n1 = float(input("Digite primeira nota do aluno: "))
+    n2 = float(input("Digite segunda nota do aluno: "))
+
+    media = (n1 + n2) / 2
+
+    print(f'a média do aluno {contador} é = {media}')
+
+    contador = contador + 1 
+
---
+++ AULA 07/ex03B.py
@@ -0,0 +1,7 @@
+for contador in range(1,11):
+    n1 = float(input("Digite primeira nota do aluno: "))
+    n2 = float(input("Digite segunda nota do aluno: "))
+
+    media = (n1 + n2) / 2
+
+    print(f'a média do aluno {contador} é = {media}')
