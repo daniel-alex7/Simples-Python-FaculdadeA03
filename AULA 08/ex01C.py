a7473a6278fe66c266bfa998f64eebc66e786ef1
Daniel Alexandre <130123798+daniel-alex7@users.noreply.github.com>
2025-04-16 19:45:24 -0300

    Codando e manipulando strings

---
+++ AULA 08/ex01C.py
@@ -0,0 +1,3 @@
+num = int(input('Digite um número inteiro: '))
+qtd = len(str(num))
+print(qtd)
---
+++ AULA 08/ex02.py
@@ -0,0 +1,8 @@
+inversa = ''
+
+string = input("Digite um texto: ")
+
+for x in string: # x equivale ao indice da string
+    inversa = inversa + x
+
+print(inversa)
